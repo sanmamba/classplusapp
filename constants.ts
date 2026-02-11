25f8d89124fab9040671ab63d5af5243930abf27
sanmamba <sivaramansanthosh.123@gmail.com>
2026-02-11 15:30:26 +0530

    update

--- constants.ts
+++ constants.ts
@@ -1,10 +1,13 @@
 import { TestData, TestMetadata } from './types';
 import module_1 from './tests/module_1.json'
+import testing123 from './tests/testing123.json'
 
 export const TESTS: TestMetadata[] = [
   { id: '1', name: 'Module 1', fileName: 'module_1.json' },
+  { id: '2', name: 'Testing 123', fileName: 'testing123.json' },
 ];
 
 export const MOCK_TEST_DATA: Record<string, TestData> = {
-  'module_1.json': module_1 as TestData
+  'module_1.json': module_1 as TestData,
+  'testing123.json': testing123 as TestData
 };
