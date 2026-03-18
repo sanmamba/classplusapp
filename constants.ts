5c8641bc0c53e56bdee902ed458df3a9b9cfbd3e
sanmamba <sivaramansanthosh.123@gmail.com>
2026-03-18 21:35:01 +0530

    adv p2

--- constants.ts
+++ constants.ts
@@ -13,6 +13,7 @@ import module_7 from './tests/module_7.json'
 import module_8 from './tests/module_8.json'
 import module_9 from './tests/module_9.json'
 import Advp1_18_03_2026 from './tests/Advp1_18_03_2026.json'
+import Advp2_18_03_2026 from './tests/Advp2_18_03_2026.json'
 
 export const TESTS: TestMetadata[] = [
   { id: '1', name: 'Testing 123', fileName: 'testing123.json' },
@@ -29,6 +30,7 @@ export const TESTS: TestMetadata[] = [
   { id: '12', name: 'Module 8', fileName: 'module_8.json' },
   { id: '13', name: 'Module 9', fileName: 'module_9.json' },
   { id: '14', name: 'Advp1_18_03_2026', fileName: 'Advp1_18_03_2026.json' },
+  { id: '15', name: 'Advp2_18_03_2026', fileName: 'Advp2_18_03_2026.json' },
 ];
 
 export const MOCK_TEST_DATA: Record<string, TestData> = {
@@ -46,4 +48,5 @@ export const MOCK_TEST_DATA: Record<string, TestData> = {
   'module_8.json': module_8 as TestData,
   'module_9.json': module_9 as TestData,
   'Advp1_18_03_2026.json': Advp1_18_03_2026 as TestData,
+  'Advp2_18_03_2026.json': Advp2_18_03_2026 as TestData,
 };
